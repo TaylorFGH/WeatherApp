5d3874599f105a9ed59591afe34ec658e2b2b0e7
zDknry <100361841+TaylorFGH@users.noreply.github.com>
2025-01-03 16:40:04 +0000

    GUI & API stuff Started

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 public class WeatherAppGui extends JFrame {
@@ -14,5 +18,83 @@ public class WeatherAppGui extends JFrame {
         setLayout(null);
 
         setResizable(false);
+
+        addGUIComponents();
+    }
+
+    
+    private void addGUIComponents() {
+
+        JTextField searchTextField = new JTextField();
+
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
+
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        //WEATHER STUFF
+        JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        // TEMPATURE STUFF
+        JLabel tempatureText = new JLabel("10 C");
+        tempatureText.setBounds(0, 350, 450, 54);
+        tempatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+
+        // CENTER IT
+        tempatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(tempatureText);
+
+        // WEATHER DESC
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0, 405, 450, 36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
+        //humidity
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+        add(humidityImage);
+
+        // humidity text
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityText);
+
+        //windspeed img
+        JLabel windspeedImage = new JLabel(loadImage("src/assets/wind.png"));
+        windspeedImage.setBounds(220, 500, 74, 66);
+        add(windspeedImage);
+
+        //windspeed text
+        JLabel windspeedText = new JLabel("<html><b>Wind Speed</b> 15 km/h</html>");
+        windspeedText.setBounds(310, 500, 85, 55);
+        windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(windspeedText);
+
+    }
+
+    
+    private ImageIcon loadImage(String resourcePath) {
+        try{
+
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+            return new ImageIcon(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Failed to load image from path:");
+        return null;
     }
 }
