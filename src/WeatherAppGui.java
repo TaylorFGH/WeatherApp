8740dd4ef7db03d8e0ffffed7975a62c26017fd4
Taylor <100361841+TaylorFGH@users.noreply.github.com>
2025-01-03 15:33:16 +0000

    Start of project

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui() {
+        
+        super("Weather App");
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setSize(450, 650);
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(false);
+    }
+}
